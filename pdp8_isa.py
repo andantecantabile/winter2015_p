08f50ab70f8ef74b9609435e107b54cadef00143
Elizabeth R <ereed@pdx.edu>
2015-03-26 21:51:15 -0700

    AND through ISZ opcodes

--- pdp8_isa.py
+++ pdp8_isa.py
@@ -60,17 +60,6 @@ opcode_name = {
     6: 'IO',
     7: 'UI'
 }
-# Opcode function calls
-opcode_type = {
-	0: op_and(),
-	1: op_tad(),
-	2: op_isz(),
-	3: op_dca(),
-	4: op_jms(),
-	5: op_jmp(),
-	6: op_io(),
-	7: op_ui
-}
 # Number of cycles required for each opcode type
 opcode_cycles = {
 	0: 2	# AND = 2 cycles
@@ -96,6 +85,7 @@ class PDP8(object):
 		self.memvalid = list()	# corresponding valid bits
 		self.curr_opcode_str = 'NOP'	# the string of the current opcode 
 		self.opcode = 0			# integer value of current opcode
+		self.word_mask = (1 << PDP8_WORD_SIZE) - 1
 		# Current values of all registers
 		# and machine state, including effective address, 
 		# memory at effective address, and the address of 
@@ -108,7 +98,28 @@ class PDP8(object):
 		self.eaddr = 0		# effective address
 		self.mem_eaddr = 0	# value in memory @ eaddr
 		self.prevPC = 0		# PC of the current(last executed) instruction
+		
+		# locations last accessed in memory; used in GUI
+		self.mem_ref = {
+			'eaddr_read': -1,
+			'eaddr_write': -1, 
+			'mem_read': -1,
+			'mem_write': -1,
+			'instr_fetch': -1
+		}	
 
+		# Opcode function calls
+		self.opcode_exec = {
+			0: self.op_and,
+			1: self.op_tad,
+			2: self.op_isz,
+			3: self.op_dca,
+			4: self.op_jms,
+			5: self.op_jmp,
+			6: self.op_io,
+			7: self.op_ui
+		}
+		
 		# Statistic tracking dictionaries for instruction 
 		# counts, total cycles used, and total numbers 
 		# of branches taken/not taken.
@@ -118,8 +129,8 @@ class PDP8(object):
 			'TAD': 0,
 			'ISZ': 0,
 			'DCA': 0,
-			'JMP': 0,
 			'JMS': 0,
+			'JMP': 0,
 			'IO': 0,
 			'UI': 0
 		}
@@ -129,16 +140,16 @@ class PDP8(object):
 			'TAD': 0,
 			'ISZ': 0,
 			'DCA': 0,
-			'JMP': 0,
 			'JMS': 0,
+			'JMP': 0,
 			'IO': 0,
 			'UI': 0
 		}
 		# counts for taken unconditional branches
 		self.branch_uncond_count = {
 			'all': 0,
-			'JMP': 0,
 			'JMS': 0,
+			'JMP': 0,
 			'UI': 0
 		}
 		# counts for taken conditional branches
@@ -154,141 +165,225 @@ class PDP8(object):
 			'UI': 0 
 		}
 
-		#===============================================
-		# CLASS FUNCTIONS
-		#------------
+	#===============================================
+	# CLASS FUNCTIONS
+	#------------
 
-		# Function: open_tracefiles
-		# Description: Opens trace files for appending.
-		def open_tracefiles(self):
-			self.tracefile = open(TRACE_FILE_NAME,'a')
-			self.branchfile = open(BRANCH_FILE_NAME,'a')
+	# Function: open_tracefiles
+	# Description: Opens trace files for appending.
+	def open_tracefiles(self):
+		self.tracefile = open(TRACE_FILE_NAME,'a')
+		self.branchfile = open(BRANCH_FILE_NAME,'a')
 
-		# Function: close_tracefiles
-		# Description: Closes trace files
-		def close_tracefiles(self):
-			self.tracefile.close()
-			self.branchfile.close()
+	# Function: close_tracefiles
+	# Description: Closes trace files
+	def close_tracefiles(self):
+		self.tracefile.close()
+		self.branchfile.close()
 
-		#-----------------------------------
-		# Function: load_memory
-		# Description: Takes input filename as parameter, and 
-		#   assigns values accordingly to the main memory array.
-		def load_memory(self,filename):
-			self.mem = list()	# initialize mem and memvalid to empty lists
-			self.memvalid = list()
-			# set PC to the starting address
-			self.PC = START_ADDR
-			# clear other registers
-			self.AC = 0
-			self.LR = 0
-			self.IR = 0
-			curr_addr = 0
-			# Set all valid bits to 0
-			for i in range (MEM_SIZE - 1)
-				self.memvalid.append(0)
-			# read from file
-			srcfile = open(filename)
-			line = srcfile.readline()
-			# Set starting address to the first address given in the file
+	#-----------------------------------
+	# Function: load_memory
+	# Description: Takes input filename as parameter, and 
+	#   assigns values accordingly to the main memory array.
+	def load_memory(self,filename):
+		self.mem = list()	# initialize mem and memvalid to empty lists
+		self.memvalid = list()
+		# set PC to the starting address
+		self.PC = START_ADDR
+		# clear other registers
+		self.AC = 0
+		self.LR = 0
+		self.IR = 0
+		curr_addr = 0
+		# Set all valid bits to 0
+		for i in range (MEM_SIZE - 1)
+			self.memvalid.append(0)
+		# read from file
+		srcfile = open(filename)
+		line = srcfile.readline()
+		# Set starting address to the first address given in the file
+		if line_char[0] == '@':
+			self.PC = int('0x'+line_char[1:])
+		
+		# read lines until end of file is encountered
+		while line != '':
+			line_char = list(line)
+			# if current line specifies an address
 			if line_char[0] == '@':
-				self.PC = int('0x'+line_char[1:])
+				curr_addr = int('0x'+line_char[1:])
+			# otherwise, if current line specifies a memory value
+			else 
+				curr_data = int('0x'+line_char[:])
+				self.memvalid[curr_addr] = 1
+				self.mem.insert(curr_addr,curr_data)
+				# increment the current address
+				curr_addr = curr_addr + 1
 			
-			# read lines until end of file is encountered
-			while line != '':
-				line_char = list(line)
-				# if current line specifies an address
-				if line_char[0] == '@':
-					curr_addr = int('0x'+line_char[1:])
-				# otherwise, if current line specifies a memory value
-				else 
-					curr_data = int('0x'+line_char[:])
-					self.memvalid[curr_addr] = 1
-					self.mem.insert(curr_addr,curr_data)
-					# increment the current address
-					curr_addr = curr_addr + 1
-				
-				# read next line
-				line = srcfile.readline()
-			
-			# close the input file
-			srcfile.close()
+			# read next line
+			line = srcfile.readline()
+		
+		# close the input file
+		srcfile.close()
 
-		#-------------------------------------
-		# Function: read_mem
-		# Arguments: address, read_type
-		# Description: "Performs" a read operation on a location
-		#    in memory, writes to the trace file, and returns the 
-		#    value read from the given memory location.
-		def read_mem(self, address, read_type):
-			# obtain address in hexadecimal for print
-			addr_hex = hex(address)
-			addr_hex = addr_hex[2:]	 # trim the leading '0x'
-			# check if the value at the given memory address is valid
-			if (self.memvalid[address] != 1)
-				print "[Warning: Invalid memory location accessed at",addr_hex
-			# write to trace file
-			self.tracefile.write(trace_type[read_type]+' '+addr_hex)
-			# return the value from memory at given address
-			return self.mem[address]	
+	#-------------------------------------
+	# Function: read_mem
+	# Arguments: address, read_type
+	# Description: "Performs" a read operation on a location
+	#    in memory, writes to the trace file, and returns the 
+	#    value read from the given memory location.
+	def read_mem(self, address, read_type):
+		# obtain address in hexadecimal for print
+		addr_hex = hex(address)
+		addr_hex = addr_hex[2:]	 # trim the leading '0x'
+		# check if the value at the given memory address is valid
+		if (self.memvalid[address] != 1)
+			print "[Warning: Invalid memory location accessed at",addr_hex
+		# write to trace file
+		self.tracefile.write(trace_type[read_type]+' '+addr_hex)
+		# return the value from memory at given address
+		return (self.mem[address] & self.word_mask)
 
-		# Function: write_mem
-		# Arguments: address, value
-		# Description: "Performs" a write operation on a location
-		#    in memory, writes to the trace file, and updates the 
-		#    value at the given memory location.
-		def write_mem(self, address, value):
-			# obtain address in hexadecimal for print
-			addr_hex = hex(address)
-			addr_hex = addr_hex[2:]	 # trim the leading '0x'
-			# write to trace file
-			self.tracefile.write(trace_type['WRITE']+' '+addr_hex)
-			# update value in the memory array & set valid bit
-			self.mem[address] = value
-			self.memvalid[address] = 1
+	#-------------------------------------
+	# Function: write_mem
+	# Arguments: address, value
+	# Description: "Performs" a write operation on a location
+	#    in memory, writes to the trace file, and updates the 
+	#    value at the given memory location.
+	def write_mem(self, address, value):
+		# obtain address in hexadecimal for print
+		addr_hex = hex(address)
+		addr_hex = addr_hex[2:]	 # trim the leading '0x'
+		# write to trace file
+		self.tracefile.write(trace_type['WRITE']+' '+addr_hex)
+		# update value in the memory array & set valid bit
+		self.mem[address] = value
+		self.memvalid[address] = 1
 
-		# Function: calc_eaddr
-		# Description: Given the current instruction, if it is an 
-		#    instruction that involves memory reference, calculates
-		#    the effective address to be used.
-		def calc_eaddr(self):
-			new_eaddr = 0
-			return new_eaddr
+	#-------------------------------------
+	# Function: calc_eaddr
+	# Description: Given the current instruction, if it is an 
+	#    instruction that involves memory reference, calculates
+	#    the effective address to be used.
+	def calc_eaddr(self):
+		new_eaddr = 0
+		return new_eaddr
 
-		# Function: exec_microinstr
-		# Description: Executes the current microinstruction.
-		def exec_microinstr(self):
-			# do stuff here
-
-		# Function: execute
-		# Description: Executes the next instruction (found at the
-		#    address specified by the PC).
-		# Return Value: 0 -> execution completed successfully,
-		#                    and no HLT was encountered
-		#               1 -> HLT microinstruction was given.
-		def execute(self):
-			# STEP 1: Fetch the current instruction, increment PC
-			self.IR = self.read_mem(self.PC,'FETCH')
-			self.prevPC = self.PC 
+	#-------------------------------------	
+	# Function: op_and
+	# Description: Executes the AND operation.
+	def op_and(self):
+		# First read the value at eaddr
+		self.mem_eaddr = read_mem(self,self.eaddr,'READ')
+		self.mem_ref['eaddr_read'] = self.eaddr
+		# Calculate AC & mem_val
+		self.AC = self.AC & self.mem_eaddr
+		
+	#-------------------------------------	
+	# Function: op_tad
+	# Description: Executes the TAD operation.
+	def op_tad(self):
+		# First read the value at eaddr
+		self.mem_eaddr = read_mem(self,self.eaddr,'READ')
+		self.mem_ref['eaddr_read'] = self.eaddr
+		# Add AC and mem_val
+		sum = self.AC + self.mem_eaddr
+		# if there is overflow from the MSbit position,
+		# invert the LR 
+		if ( (sum >> PDP8_WORD_SIZE) != 0 ):
+			self.LR = not(self.LR)  
+			# Note: Using logical NOT here, since LR is one bit only
+		# save the AC using mask
+		self.AC = sum & self.word_mask 
+	
+	#-------------------------------------	
+	# Function: op_isz
+	# Description: Executes the ISZ operation.
+	def op_isz(self):
+		# First read the value at eaddr
+		self.mem_eaddr = read_mem(self,self.eaddr,'READ')
+		self.mem_ref['eaddr_read'] = self.eaddr
+		# Increment value by 1
+		self.mem_eaddr = (self.mem_eaddr + 1) & self.word_mask
+		# Write updated value to memory
+		self.write_mem(self,self.eaddr,self.mem_eaddr)
+		self.mem_ref['eaddr_write'] = self.eaddr
+		# Check if the updated value is 0: if so, skip next instr
+		if self.mem_eaddr = 0:
 			self.PC = self.PC + 1
+			self.branch_trace(self)
+		else:
+			self.branch_trace(self)
+	
+	#-------------------------------------	
+	# Function: op_dca
+	# Description: Executes the DCA operation.
+	def op_dca(self):
+	
+	#-------------------------------------	
+	# Function: op_jms
+	# Description: Executes the JMS operation.
+	def op_jms(self):
+	
+	#-------------------------------------	
+	# Function: op_jmp
+	# Description: Executes the JMP operation.
+	def op_jmp(self):
+	
+	#-------------------------------------	
+	# Function: op_io
+	# Description: Executes the IO operation.
+	def op_io(self):
+	
 
-			# STEP 2: Decode the current instruction
-			# determine the opcode
-			self.curr_opcode = self.IR >> (PDP8_WORD_SIZE - PDP8_OPCODE_SIZE)
-			self.curr_opcode_str = opcode_name[self.curr_opcode]
-			op_str = self.curr_opcode_str 	# shorter name for easier use, read-only
-			
-			# STEP 2b: Determine the Effective Address
-			if (self.curr_opcode >= 0 and self.curr_opcode < 6):
-				self.eaddr = self.calc_eaddr()
-			
-			# STEP 3: Execute Current Instruction
-			opcode_type.get(self.curr_opcode, op_default())
-			# This will call the corresponding function based on the current opcode.
-			
-			# STEP 4: Update Stats for Opcodes
-			self.instr_count['all'] = self.instr_count['all'] + 1
-			self.instr_count[self.curr_opcode_str] = self.instr_count[self.curr_opcode_str]+1
-			self.cycle_count['all'] = self.cycle_count['all'] + opcode_cycles[self.curr_opcode_str]
-			self.cycle_count[self.curr_opcode_str] = self.cycle_count[self.curr_opcode_str]+opcode_cycles[self.curr_opcode_str]
-			
+	#-------------------------------------
+	# Function: op_ui
+	# Description: Executes the current microinstruction.
+	def op_ui(self):
+		# do stuff here
+	
+	#-------------------------------------
+	# Function: op_default
+	# Description: "Executes" a NOP.
+	def op_default(self):
+		# do nothing
+
+	#-------------------------------------
+	# Function: execute
+	# Description: Executes the next instruction (found at the
+	#    address specified by the PC).
+	# Return Value: 0 -> execution completed successfully,
+	#                    and no HLT was encountered
+	#               1 -> HLT microinstruction was given.
+	def execute(self):
+		# STEP 0: Clear all locations referenced in memory by last instruction
+		self.mem_ref['eaddr_read'] = -1
+		self.mem_ref['eaddr_write'] = -1
+		self.mem_ref['mem_read'] = -1
+		self.mem_ref['mem_write'] = -1
+		
+		# STEP 1: Fetch the current instruction, increment PC
+		self.IR = self.read_mem(self,self.PC,'FETCH')
+		self.mem_ref['instr_fetch'] = self.PC	# save location of fetched instr
+		self.prevPC = self.PC 
+		self.PC = self.PC + 1
+
+		# STEP 2: Decode the current instruction
+		# determine the opcode
+		self.curr_opcode = self.IR >> (PDP8_WORD_SIZE - PDP8_OPCODE_SIZE)
+		self.curr_opcode_str = opcode_name[self.curr_opcode]
+		op_str = self.curr_opcode_str 	# shorter name for easier use, read-only
+		
+		# STEP 2b: Determine the Effective Address
+		if (self.curr_opcode >= 0 and self.curr_opcode < 6):
+			self.eaddr = self.calc_eaddr()
+		
+		# STEP 3: Execute Current Instruction
+		self.opcode_exec.get(self.curr_opcode,self.op_default)(self)
+		# This will call the corresponding function based on the current opcode.
+		
+		# STEP 4: Update Stats for Opcodes
+		self.instr_count['all'] = self.instr_count['all'] + 1
+		self.instr_count[op_str] = self.instr_count[op_str]+1
+		self.cycle_count['all'] = self.cycle_count['all'] + opcode_cycles[op_str]
+		self.cycle_count[op_str] = self.cycle_count[op_str]+opcode_cycles[op_str]
+		
