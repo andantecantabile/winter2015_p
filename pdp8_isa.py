feae19c676a6a1f5fd3f709d6a73e98bb68a471f
Elizabeth R <ereed@pdx.edu>
2015-03-27 08:40:05 -0700

    eaddr, branch_trace, all opcodes except for UI

--- pdp8_isa.py
+++ pdp8_isa.py
@@ -4,9 +4,12 @@
 ### Author: Elizabeth Reed
 ### Comments: This module contains the main functions and
 ###           data structures for the pdp-8 simulator
+### Additional features that would be good to implement later:
+###		1. Modify load_memory() so that it can parse EITHER 
+###		   input file type, not just the verilog version.
 
 # import the logarithmic function
-from math import log, bin, hex, oct
+from math import log, bin, oct, hex
 
 #----------------------------------------
 # CONSTANTS
@@ -32,25 +35,27 @@ PAGE_BITS = log(PDP8_PAGE_NUM, 2)
 # Address Indices for page and offset
 ADDR_PAGE_LOW = 0
 ADDR_PAGE_HIGH = PAGE_BITS - 1
+ADDR_PAGE_SIZE = (ADDR_PAGE_HIGH - ADDR_PAGE_LOW + 1)
 ADDR_OFFSET_LOW = PAGE_BITS
 ADDR_OFFSET_HIGH = PDP8_ADDR_SIZE - 1
+ADDR_OFFSET_SIZE = (ADDR_OFFSET_HIGH - ADDR_OFFSET_LOW + 1)
 
 # Dictionaries for memory operations saved to trace files,
 # for branch trace file operations, and opcode information.
-trace_type = {
+TRACE_TYPE = {
 	'READ': 0, 
 	'WRITE': 1, 
 	'FETCH': 2
 }
 
-branch_type = {
+BRANCH_TYPE = {
 	'NO_BRANCH': 0, 
 	'UNCONDITIONAL': 1, 
 	'CONDITIONAL': 2
 }
 
 # Opcode Names
-opcode_name = {
+OPCODE_NAME = {
     0: 'AND',
     1: 'TAD',
     2: 'ISZ',
@@ -61,15 +66,20 @@ opcode_name = {
     7: 'UI'
 }
 # Number of cycles required for each opcode type
-opcode_cycles = {
-	0: 2	# AND = 2 cycles
-	1: 2 	# TAD = 2 cycles 
-	2: 2 	# ISZ = 2 cycles
-	3: 2 	# DCA = 2 cycles 
-	4: 2 	# JMS = 2 cycles 
-	5: 1 	# JMP = 1 cycle
-	6: 0 	# IO = 0 cycles 
+OPCODE_CYCLES = {
+	0: 2,	# AND = 2 cycles
+	1: 2, 	# TAD = 2 cycles 
+	2: 2, 	# ISZ = 2 cycles
+	3: 2, 	# DCA = 2 cycles 
+	4: 2, 	# JMS = 2 cycles 
+	5: 1, 	# JMP = 1 cycle
+	6: 0, 	# IO = 0 cycles 
 	7: 1 	# UI = 1 cycle 
+}
+# Number of cycles added for effective address calculation
+EADDR_CYCLES = {
+	'AutoIndex': 2,	# Auto-Indexing: 2 additional cycles 
+	'Indirect': 1 	# Indirect Addressing: 1 additional cycle  
 }
 
 #--------------------------------------
@@ -83,7 +93,7 @@ class PDP8(object):
 		self.branchfile = ''	# branch file object
 		self.mem = list()		# the main memory array
 		self.memvalid = list()	# corresponding valid bits
-		self.curr_opcode_str = 'NOP'	# the string of the current opcode 
+		self.opcode_str = 'NOP'	# the string of the current opcode 
 		self.opcode = 0			# integer value of current opcode
 		self.word_mask = (1 << PDP8_WORD_SIZE) - 1
 		# Current values of all registers
@@ -168,6 +178,17 @@ class PDP8(object):
 	#===============================================
 	# CLASS FUNCTIONS
 	#------------
+	
+	# Function: init_tracefiles
+	# Description: Clears contents of output trace file,
+	#    prints header for the branch trace file
+	def init_tracefiles(self):
+		self.tracefile = open(TRACE_FILE_NAME,'w')
+		self.tracefile.close()
+		self.branchfile = open(BRANCH_FILE_NAME,'w')
+		self.branchfile.write('PC [octal]    BRANCH TYPE         TAKEN/NOT TAKEN    TARGET ADDRESS [octal]\n')
+		self.branchfile.write('---------------------------------------------------------------------------\n')
+		self.branchfile.close()
 
 	# Function: open_tracefiles
 	# Description: Opens trace files for appending.
@@ -239,7 +260,7 @@ class PDP8(object):
 		if (self.memvalid[address] != 1)
 			print "[Warning: Invalid memory location accessed at",addr_hex
 		# write to trace file
-		self.tracefile.write(trace_type[read_type]+' '+addr_hex)
+		self.tracefile.write(TRACE_TYPE[read_type]+' '+addr_hex+'\n')
 		# return the value from memory at given address
 		return (self.mem[address] & self.word_mask)
 
@@ -254,19 +275,114 @@ class PDP8(object):
 		addr_hex = hex(address)
 		addr_hex = addr_hex[2:]	 # trim the leading '0x'
 		# write to trace file
-		self.tracefile.write(trace_type['WRITE']+' '+addr_hex)
+		self.tracefile.write(TRACE_TYPE['WRITE']+' '+addr_hex+'\n')
 		# update value in the memory array & set valid bit
 		self.mem[address] = value
 		self.memvalid[address] = 1
 
 	#-------------------------------------
+	# Function: write_branch_trace
+	# Arguments: current PC, opcode (string), target address,
+	#    the type of branch, and a flag indicating taken/not taken.
+	# Description: Writes a line to the branch trace file and 
+	#    updates branch statistics.
+	def write_branch_trace(self, curr_PC, op_str, target_addr, 
+		branch_type, branch_taken):
+		# unconditional branch type
+		if branch_type == BRANCH_TYPE['UNCONDITIONAL']:
+			self.branch_uncond_count['all'] = self.branch_uncond_count['all'] + 1
+			self.branch_uncond_count[op_str] = self.branch_uncond_count[op_str] + 1
+			# write to trace file
+			self.branchfile.write('%04o          UNCONDITIONAL [%3s] TAKEN              %04o \n' % (curr_PC, op_str, target_addr)
+			# Example formatted print, uses a tuple: print '%d, %0d, %5d, %05d' % (34, 34, 34, 34)
+		# conditional branch type 
+		elif branch.type == BRANCH_TYPE['CONDITIONAL']:
+			if branch_taken == True:
+				self.branch_cond_t_count['all'] = self.branch_cond_t_count['all'] + 1 
+				self.branch_cond_t_count[op_str] = self.branch_cond_t_count[op_str] + 1 
+				self.branchfile.write('%04o          CONDITIONAL [%3s]    TAKEN              %04o \n' % (curr_PC, op_str, target_addr)
+			else: 
+				self.branch_cond_nt_count['all'] = self.branch_cond_nt_count['all'] + 1 
+				self.branch_cond_nt_count[op_str] = self.branch_cond_nt_count[op_str] + 1
+				self.branchfile.write('%04o          CONDITIONAL [%3s]    NOT TAKEN          %04o \n' % (curr_PC, op_str, target_addr)
+	
+	#-------------------------------------
 	# Function: calc_eaddr
 	# Description: Given the current instruction, if it is an 
 	#    instruction that involves memory reference, calculates
 	#    the effective address to be used.
+	#    Uses the current value of the prevPC, the IR, and
+	#    may read/write to mem[].
+	#    Note that this function does not verify that the 
+	#    opcode is in range of memory instructions, assumes
+	#    that the check is performed before calc_eaddr has 
+	#    been called.
 	def calc_eaddr(self):
-		new_eaddr = 0
-		return new_eaddr
+		# Convert IR to a binary string to check if specific
+		# flag bits are set.
+		IR_bin_str = bin(self.IR)
+		
+		offset_mask = (1 << (ADDR_OFFSET_SIZE)) - 1 
+		page_mask = (1 << (ADDR_PAGE_SIZE)) - 1
+		
+		current_page = self.prevPC & page_mask 
+		current_offset = self.IR & offset_mask
+		
+		eff_addr = -1	# initialize return val to -1
+		
+		# Check if the Indirect Addressing bit is not set
+		if IR_bin_str[ADDR_INDIRECT_ADDR_BIT] == '0':
+			# If Indirect Addressing bit = 0 (Direct addressing) 
+			# Check if the Memory Page bit is set...
+			if IR_bin_str[ADDR_MEMORY_PAGE_BIT] == '0':
+				# ADDRESSING MODE 1: Zero Page Addressing
+				#    Effective Address <- 00000 + Offset
+				eff_addr = current_offset
+			else:	# Memory Page bit = 1
+				# ADDRESSING MODE 2: Current Page Addressing
+				#    Effective Address <- Old_PC[0:4] + Offset
+				eff_addr = current_page + current_offset
+		else: # Indirect Addressing bit = 1 
+			# Check if the AutoIndex registers are not selected.
+			# (locations outside of 0010 through 0017 (octal) have been indicated)
+			if ( (current_offset < 0o10) or (current_offset > 0o17) ):
+				# ADDRESSING MODE 3: Indirect Addressing
+				# Check if the memory bit is set 
+				if IR_bin_str[ADDR_MEMORY_PAGE_BIT] == '0':
+					# Zero Page Addressing, so: 
+					#    Effective Address <- C(00000 + Offset)
+					indirect_addr_loc = current_offset
+					# Read memory at indirect_addr_loc
+					eff_addr = self.read_mem(self,indirect_addr_loc,'READ')
+					self.mem_ref['eaddr_read'] = indirect_addr_loc
+				else:
+					# Current Page Addressing, so:
+					#    Effective Address <- C(Old_PC[0:4] + Offset)
+					indirect_addr_loc = current_page + current_offset
+					# Read memory at indirect_addr_loc
+					eff_addr = self.read_mem(self,indirect_addr_loc,'READ')
+					self.mem_ref['eaddr_read'] = indirect_addr_loc
+				# Add cycles for use of Indirect Addressing
+				self.cycle_count['all'] = self.cycle_count['all'] + EADDR_CYCLES['Indirect']
+				self.cycle_count[self.opcode_str] = self.cycle_count[self.opcode_str]+EADDR_CYCLES['Indirect']
+			else: # offset location in range of 0010 - 0017 (octal) was specified
+				# ADDRESSING MODE 4: AutoIndex Addressing
+				#      C(AutoIndex_Register) <- C(AutoIndex_Register) + 1
+				#      EffectiveAddress <- C(AutoIndex_Register)
+				indirect_addr_loc = current_offset
+				# Read memory at indirect_addr_loc
+				eff_addr = self.read_mem(self,indirect_addr_loc,'READ')
+				self.mem_ref['eaddr_read'] = indirect_addr_loc
+				# Increment the value 
+				eff_addr = eff_addr + 1 
+				# Store the incremented value in memory
+				self.write_mem(self,indirect_addr_loc,eff_addr)
+				self.mem_ref['eaddr_write'] = indirect_addr_loc
+				# Add cycles for use of AutoIndex Addressing
+				self.cycle_count['all'] = self.cycle_count['all'] + EADDR_CYCLES['AutoIndex']
+				self.cycle_count[self.opcode_str] = self.cycle_count[self.opcode_str]+EADDR_CYCLES['AutoIndex']
+		# Return the calculated effective address
+		return eff_addr
 
 	#-------------------------------------	
 	# Function: op_and
@@ -274,7 +390,7 @@ class PDP8(object):
 	def op_and(self):
 		# First read the value at eaddr
 		self.mem_eaddr = read_mem(self,self.eaddr,'READ')
-		self.mem_ref['eaddr_read'] = self.eaddr
+		self.mem_ref['mem_read'] = self.eaddr
 		# Calculate AC & mem_val
 		self.AC = self.AC & self.mem_eaddr
 		
@@ -284,7 +400,7 @@ class PDP8(object):
 	def op_tad(self):
 		# First read the value at eaddr
 		self.mem_eaddr = read_mem(self,self.eaddr,'READ')
-		self.mem_ref['eaddr_read'] = self.eaddr
+		self.mem_ref['mem_read'] = self.eaddr
 		# Add AC and mem_val
 		sum = self.AC + self.mem_eaddr
 		# if there is overflow from the MSbit position,
@@ -301,51 +417,79 @@ class PDP8(object):
 	def op_isz(self):
 		# First read the value at eaddr
 		self.mem_eaddr = read_mem(self,self.eaddr,'READ')
-		self.mem_ref['eaddr_read'] = self.eaddr
+		self.mem_ref['mem_read'] = self.eaddr
 		# Increment value by 1
 		self.mem_eaddr = (self.mem_eaddr + 1) & self.word_mask
 		# Write updated value to memory
 		self.write_mem(self,self.eaddr,self.mem_eaddr)
-		self.mem_ref['eaddr_write'] = self.eaddr
+		self.mem_ref['mem_write'] = self.eaddr
 		# Check if the updated value is 0: if so, skip next instr
 		if self.mem_eaddr = 0:
 			self.PC = self.PC + 1
-			self.branch_trace(self)
+			# Write to branch trace file, providing current PC, opcode string,
+			# target address, branch type, and flag indicating branch taken/not taken
+			self.write_branch_trace(self, self.prevPC, self.opcode_str, 
+				self.PC, BRANCH_TYPE['CONDITIONAL'], True)
 		else:
-			self.branch_trace(self)
+			self.write_branch_trace(self, self.prevPC, self.opcode_str, 
+				self.PC, BRANCH_TYPE['CONDITIONAL'], False)
 	
 	#-------------------------------------	
 	# Function: op_dca
 	# Description: Executes the DCA operation.
 	def op_dca(self):
+		# Write AC to memory
+		self.write_mem(self,self.eaddr,self.AC)
+		self.mem_ref['mem_write'] = self.eaddr
+		# Clear the AC
+		self.AC = 0 
 	
 	#-------------------------------------	
 	# Function: op_jms
 	# Description: Executes the JMS operation.
 	def op_jms(self):
+		# Save the PC to the effective address location
+		# (Recall that PC was already incremented in step 1, 
+		#  so it is already pointing to the next instruction)
+		self.write_mem(self,self.eaddr,self.PC)
+		self.mem_ref['mem_write'] = self.eaddr
+		# Set the PC to (EffAddr + 1)
+		self.PC = (self.eaddr + 1) & self.word_mask 
+		# Write to the branch trace file, providing current PC, opcode string,
+		# target address, branch type, and flag indicating branch taken/not taken
+		self.write_branch_trace(self, self.prevPC, self.opcode_str, 
+				self.PC, BRANCH_TYPE['UNCONDITIONAL'], True)
 	
 	#-------------------------------------	
 	# Function: op_jmp
 	# Description: Executes the JMP operation.
 	def op_jmp(self):
+		# Set PC to the effective address
+		self.PC = self.eaddr
+		# Write to the branch trace file, providing current PC, opcode string,
+		# target address, branch type, and flag indicating branch taken/not taken
+		self.write_branch_trace(self, self.prevPC, self.opcode_str, 
+				self.PC, BRANCH_TYPE['UNCONDITIONAL'], True)
 	
 	#-------------------------------------	
 	# Function: op_io
 	# Description: Executes the IO operation.
 	def op_io(self):
-	
+		# Not implemented, print warning
+		print 'WARNING: IO instruction encountered at PC: [%04o]\n' % self.prevPC
 
 	#-------------------------------------
 	# Function: op_ui
 	# Description: Executes the current microinstruction.
 	def op_ui(self):
-		# do stuff here
+		# 
 	
 	#-------------------------------------
 	# Function: op_default
 	# Description: "Executes" a NOP.
 	def op_default(self):
-		# do nothing
+		# do nothing, print warning that illegal opcode was given
+		print 'WARNING: Invalid opcode encountered at PC: [%04o]\n' % self.prevPC
 
 	#-------------------------------------
 	# Function: execute
@@ -369,21 +513,21 @@ class PDP8(object):
 
 		# STEP 2: Decode the current instruction
 		# determine the opcode
-		self.curr_opcode = self.IR >> (PDP8_WORD_SIZE - PDP8_OPCODE_SIZE)
-		self.curr_opcode_str = opcode_name[self.curr_opcode]
-		op_str = self.curr_opcode_str 	# shorter name for easier use, read-only
+		self.opcode = self.IR >> (PDP8_WORD_SIZE - PDP8_OPCODE_SIZE)
+		self.opcode_str = OPCODE_NAME[self.opcode]
+		op_str = self.opcode_str 	# shorter name for easier use, read-only
 		
 		# STEP 2b: Determine the Effective Address
-		if (self.curr_opcode >= 0 and self.curr_opcode < 6):
+		if (self.opcode >= 0 and self.opcode < 6):
 			self.eaddr = self.calc_eaddr()
 		
 		# STEP 3: Execute Current Instruction
-		self.opcode_exec.get(self.curr_opcode,self.op_default)(self)
+		self.opcode_exec.get(self.opcode,self.op_default)(self)
 		# This will call the corresponding function based on the current opcode.
 		
 		# STEP 4: Update Stats for Opcodes
 		self.instr_count['all'] = self.instr_count['all'] + 1
 		self.instr_count[op_str] = self.instr_count[op_str]+1
-		self.cycle_count['all'] = self.cycle_count['all'] + opcode_cycles[op_str]
-		self.cycle_count[op_str] = self.cycle_count[op_str]+opcode_cycles[op_str]
+		self.cycle_count['all'] = self.cycle_count['all'] + OPCODE_CYCLES[op_str]
+		self.cycle_count[op_str] = self.cycle_count[op_str]+OPCODE_CYCLES[op_str]
 		
